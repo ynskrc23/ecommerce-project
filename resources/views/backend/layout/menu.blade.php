a7a6d009d13e9edc92bba4c9059903183f0cccb2
Yunus Karaca <ynskrc82@gmail.com>
2022-11-27 20:02:53 +0300

    header, footer, topbar ve menu dosyaları oluşturldu

---
+++ resources/views/backend/layout/menu.blade.php
@@ -0,0 +1,36 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index">
+        <div class="sidebar-brand-text mx-3">Ecommerce Panel</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-home"></i>
+            <span> Yönetim Paneli</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="/users">
+            <i class="fas fa-user"></i>
+            <span> Kullanıcılar</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -1,313 +1,95 @@
-<!DOCTYPE html>
-<html lang="en">
+@include('backend.layout.header')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<!-- Topbar -->
+@include('backend.layout.topbar')
+<!-- End of Topbar -->
 
-    <title>Laravel Dashboard</title>
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-    <!-- Custom fonts for this template -->
-    <link href="{{ asset("backend/vendor/fontawesome-free/css/all.min.css") }}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Yönetim Paneli</h1>
 
-    <!-- Custom styles for this template -->
-    <link href="{{ asset("backend/css/sb-admin-2.min.css") }}" rel="stylesheet">
-
-    <!-- Custom styles for this page -->
-    <link href="{{ asset("backend/vendor/datatables/dataTables.bootstrap4.min.css") }}" rel="stylesheet">
-
-</head>
-
-<body id="page-top">
-
-<!-- Page Wrapper -->
-<div id="wrapper">
-
-    <!-- Sidebar -->
-    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-        <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index">
-            <div class="sidebar-brand-text mx-3">Laravel Ecommerce </div>
-        </a>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider my-0">
-
-        <!-- Nav Item - Dashboard -->
-        <li class="nav-item">
-            <a class="nav-link" href="">
-                <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span> Yönetim Paneli</span>
-            </a>
-        </li>
-
-        <!-- Nav Item - Pages Collapse Menu -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-               aria-expanded="true" aria-controls="collapseTwo">
-                <i class="fas fa-fw fa-cog"></i>
-                <span>Kullanıcılar</span>
-            </a>
-            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Custom Components:</h6>
-                    <a class="collapse-item" href="buttons.html">Buttons</a>
-                    <a class="collapse-item" href="cards.html">Cards</a>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <h6 class="m-0 font-weight-bold text-primary">Kullanıcılar</h6>
+                </div>
+                <div class="col-md-6">
+                    <a href="{{ url("/users/create") }}" class="btn btn-sm btn-success btn-icon-split float-right">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-plus"></i>
+                            </span>
+                        <span class="text">Yeni Ekle</span>
+                    </a>
                 </div>
             </div>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
-
-        <!-- Sidebar Toggler (Sidebar) -->
-        <div class="text-center d-none d-md-inline">
-            <button class="rounded-circle border-0" id="sidebarToggle"></button>
-        </div>
-
-    </ul>
-    <!-- End of Sidebar -->
-
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
-
-        <!-- Main Content -->
-        <div id="content">
-
-            <!-- Topbar -->
-            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                <!-- Sidebar Toggle (Topbar) -->
-                <form class="form-inline">
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                </form>
-
-                <!-- Topbar Search -->
-                <form
-                    class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                    <div class="input-group">
-                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                               aria-label="Search" aria-describedby="basic-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" type="button">
-                                <i class="fas fa-search fa-sm"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-
-                <!-- Topbar Navbar -->
-                <ul class="navbar-nav ml-auto">
-
-                    <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                    <li class="nav-item dropdown no-arrow d-sm-none">
-                        <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-search fa-fw"></i>
-                        </a>
-                        <!-- Dropdown - Messages -->
-                        <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                             aria-labelledby="searchDropdown">
-                            <form class="form-inline mr-auto w-100 navbar-search">
-                                <div class="input-group">
-                                    <input type="text" class="form-control bg-light border-0 small"
-                                           placeholder="Search for..." aria-label="Search"
-                                           aria-describedby="basic-addon2">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary" type="button">
-                                            <i class="fas fa-search fa-sm"></i>
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </li>
-
-                    <div class="topbar-divider d-none d-sm-block"></div>
-
-                    <!-- Nav Item - User Information -->
-                    <li class="nav-item dropdown no-arrow">
-                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
-                            <img class="img-profile rounded-circle" src="{{ asset("backend/img/undraw_profile.svg") }}">
-                        </a>
-                        <!-- Dropdown - User Information -->
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#">
-                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Profil
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                Çıkış
-                            </a>
-                        </div>
-                    </li>
-
-                </ul>
-
-            </nav>
-            <!-- End of Topbar -->
-
-            <!-- Begin Page Content -->
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <h1 class="h3 mb-2 text-gray-800">Yönetim Paneli</h1>
-
-                <!-- DataTales Example -->
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <h6 class="m-0 font-weight-bold text-primary">Kullanıcılar</h6>
-                            </div>
-                            <div class="col-md-6">
-                                <a href="{{ url("/users/create") }}" class="btn btn-sm btn-success btn-icon-split float-right">
-                                        <span class="icon text-white-50">
-                                            <i class="fas fa-plus"></i>
-                                        </span>
-                                    <span class="text">Yeni Ekle</span>
-                                </a>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Ad Soyad</th>
-                                        <th>Email</th>
-                                        <th>Durum</th>
-                                        <th>İşlemler</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    @if(count($users) > 0)
-                                        @foreach($users as $user)
-                                            <tr>
-                                                <td>{{$loop->iteration}}</td>
-                                                <td>{{$user->name}}</td>
-                                                <td>{{$user->email}}</td>
-                                                <td>
-                                                    @if ($user->is_active == 1)
-                                                        <span class="badge bg-success text-white">Aktif</span>
-                                                    @else
-                                                        <span class="badge bg-danger text-white">Pasif</span>
-                                                    @endif
-                                                </td>
-                                                <td>
-                                                    <ul class="list-group" style="display:block;">
-                                                        <a href="{{ url("/users/$user->user_id/edit") }}" class="btn btn-sm btn-primary btn-icon-split">
-                                                            <span class="icon text-white-50">
-                                                                <i class="fas fa-edit"></i>
-                                                            </span>
-                                                            <span class="text">Düzenle</span>
-                                                        </a>
-                                                        <a href="{{ url("/users/$user->user_id") }}" class="btn btn-sm btn-danger btn-icon-split list-item-delete">
-                                                            <span class="icon text-white-50">
-                                                                <i class="fas fa-trash"></i>
-                                                            </span>
-                                                            <span class="text">Sil</span>
-                                                        </a>
-                                                        <a href="#" class="btn btn-sm btn-info btn-icon-split">
-                                                            <span class="icon text-white-50">
-                                                                <i class="fas fa-lock"></i>
-                                                            </span>
-                                                            <span class="text">Şifre Değiştir</span>
-                                                        </a>
-                                                    </ul>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    @else
-                                        <tr>
-                                            <td colspan="5" class="text-center">Herhangi bir kullanıcı bulunamadı.</td>
-                                        </tr>
-                                    @endif
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <!-- /.container-fluid -->
 
         </div>
-        <!-- End of Main Content -->
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Ad Soyad</th>
+                            <th>Email</th>
+                            <th>Durum</th>
+                            <th>İşlemler</th>
+                        </tr>
+                    </thead>
 
-        <!-- Footer -->
-        <footer class="sticky-footer bg-white">
-            <div class="container my-auto">
-                <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Your Website 2022</span>
-                </div>
-            </div>
-        </footer>
-        <!-- End of Footer -->
-
-    </div>
-    <!-- End of Content Wrapper -->
-
-</div>
-<!-- End of Page Wrapper -->
-
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
-
-<!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="login.html">Logout</a>
+                    <tbody>
+                        @if(count($users) > 0)
+                            @foreach($users as $user)
+                                <tr>
+                                    <td>{{$loop->iteration}}</td>
+                                    <td>{{$user->name}}</td>
+                                    <td>{{$user->email}}</td>
+                                    <td>
+                                        @if ($user->is_active == 1)
+                                            <span class="badge bg-success text-white">Aktif</span>
+                                        @else
+                                            <span class="badge bg-danger text-white">Pasif</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <ul class="list-group" style="display:block;">
+                                            <a href="{{ url("/users/$user->user_id/edit") }}" class="btn btn-sm btn-primary btn-icon-split">
+                                                <span class="icon text-white-50">
+                                                    <i class="fas fa-edit"></i>
+                                                </span>
+                                                <span class="text">Düzenle</span>
+                                            </a>
+                                            <a href="{{ url("/users/$user->user_id") }}" class="btn btn-sm btn-danger btn-icon-split list-item-delete">
+                                                <span class="icon text-white-50">
+                                                    <i class="fas fa-trash"></i>
+                                                </span>
+                                                <span class="text">Sil</span>
+                                            </a>
+                                            <a href="#" class="btn btn-sm btn-info btn-icon-split">
+                                                <span class="icon text-white-50">
+                                                    <i class="fas fa-lock"></i>
+                                                </span>
+                                                <span class="text">Şifre Değiştir</span>
+                                            </a>
+                                        </ul>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="5" class="text-center">Herhangi bir kullanıcı bulunamadı.</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+
 </div>
-
-<!-- Bootstrap core JavaScript-->
-<script src="{{ asset("backend/vendor/jquery/jquery.min.js") }}"></script>
-<script src="{{ asset("backend/vendor/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="{{ asset("backend/vendor/jquery-easing/jquery.easing.min.js") }}"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="{{ asset("backend/js/sb-admin-2.min.js") }}"></script>
-
-<!-- Page level plugins -->
-<script src="{{ asset("backend/vendor/datatables/jquery.dataTables.min.js") }}"></script>
-<script src="{{ asset("backend/vendor/datatables/dataTables.bootstrap4.min.js") }}"></script>
-
-<!-- Page level custom scripts -->
-<script src="{{ asset("backend/js/demo/datatables-demo.js") }}"></script>
-
-</body>
-
-</html>
+<!-- /.container-fluid -->
+@include('backend.layout.footer')
